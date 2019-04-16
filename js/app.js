1d0e7087387f7ae85f7180cfbfcdf7ae1891b6e5
Paweł <pawel.rzepinski@gmail.com>
2019-04-16 12:46:27 +0200

    article section

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-// parallax
 
-$(window).scroll(function () {
-    $("body").css("background-position","50% " + ($(this).scrollTop() / 2) + "px");
-});
+//     // parallax
+
+//     $(window).scroll(function () {
+//         $("body").css("background-position","50% " + ($(this).scrollTop() / 2) + "px");
+//     });
+
+//     // menu
+// $(function(){
+//     const $menuBtn = document.querySelector(".menuBtn");
+//     const $menu = document.querySelector(".popup");
+
+//     $menuBtn.on("click", function(){
+//         console.log('działa');
+        
+//         // $menu.css.display = "flex";
+//     })
+// })
+    
+
 
 
