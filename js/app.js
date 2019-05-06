15fafe46dbf31a20f420377a8eaf16e76b05b6be
Paweł <pawel.rzepinski@gmail.com>
2019-05-06 12:25:24 +0200

    js details

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
+const menuBtn = document.querySelector(".menuBtn");
+const menuList = document.querySelector(".popup");
+const delateBtn = document.querySelector(".delateBtn");
+const menuLi = document.querySelectorAll(".menuLi");
 
-    // parallax
+console.log(menuLi);
 
-    $(window).scroll(function () {
-        $("body").css("background-position","50% " + ($(this).scrollTop() / 2) + "px");
-    });
+// parallax
+$(window).scroll(function () {
+    $("body").css("background-position","50% " + ($(this).scrollTop() / 2) + "px");
+});
 
-//     // menu
-// $(function(){
-//     const $menuBtn = document.querySelector(".menuBtn");
-//     const $menu = document.querySelector(".popup");
 
-//     $menuBtn.on("click", function(){
-//         console.log('działa');
-        
-//         // $menu.css.display = "flex";
-//     })
-// })
     
+const menuOpen = function (){
+    menuList.style.display = "flex";
+}
 
+const menuClose = function(){
+    menuList.style.display = "none";
+}
 
+const menuExit = function (){
+    menuList.style.display = "none";
+    console.log('działą event');
+}
 
+menuBtn.addEventListener("click", menuOpen);
+delateBtn.addEventListener("click", menuClose);
+menuLi.forEach(function(el){
+    el.addEventListener("click", menuExit);
+})
